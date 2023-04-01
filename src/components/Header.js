6f50b2f8d0d224a2b5045fa9e5b8e0b58d35a225
Priscilla <comptepro@mbpdepriscilla.home>
2023-04-01 13:21:37 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = ({ setVisible, visible }) => {
+  return (
+    <header>
+      <button
+        onClick={() => {
+          setVisible(!visible); // on inverse la valeur de `visible` à chaque click
+        }}
+      >
+        Afficher/Masquer Modal
+      </button>
+    </header>
+  )
+}
+
+export default Header;
